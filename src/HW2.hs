80c9a5c2f2047e08e258ef87772ad80987aaac86
Brian Chung <brianchung23@gmail.com>
2017-06-12 19:13:12 -0700

    Refactor parseErrorr to use Maybe

--- src/HW2.hs
+++ src/HW2.hs
@@ -10,17 +10,18 @@ parseMessage = parseMessage' . words
 
 parseMessage' :: [String] -> LogMessage
 parseMessage' l @ ("I":xs) = fromMaybe (Unknown $ unwords l) $ parseInfo xs
-parseMessage' ("E":xs) = parseError xs
+parseMessage' l @ ("E":xs) = fromMaybe (Unknown $ unwords l) $ parseError xs
 parseMessage' a @ _ = Unknown $ unwords a
-
--- I 147 mice in the air, I’m afraid, but you might catch a bat, and
 
 parseInfo :: [String] -> Maybe LogMessage
 parseInfo (t:xs) = (\x -> LogMessage Info x (unwords xs)) <$> readMaybe t
 parseInfo _ = Nothing
 -- parseInfo (t:xs) =  fmap (\x -> (LogMessage Info x (unwords xs))) $ readMaybe t
 
-parseError :: [String] -> LogMessage
-parseError (e:xs) = LogMessage (Error $ read e) (read t) (unwords xs')
+zipMaybe :: Maybe a -> Maybe b -> Maybe (a, b)
+zipMaybe a b = a >>= (\a' -> (\b' -> (a', b')) <$> b)
+
+parseError :: [String] -> Maybe LogMessage
+parseError (e:xs) = (\(a, b) -> LogMessage (Error a) b (unwords xs')) <$> zipMaybe (readMaybe e :: Maybe Int) (readMaybe t :: Maybe Int)
   where (t:xs') = xs
-parseError a @ _ = Unknown $ unwords a
+parseError _ = Nothing
--- test/HW2Spec.hs
+++ test/HW2Spec.hs
@@ -11,9 +11,11 @@ spec = describe "parseMessage" $ do
         parseMessage "I 147 mice in the air, I’m afraid, but you might catch a bat, and" `shouldBe` LogMessage Info 147 "mice in the air, I’m afraid, but you might catch a bat, and"
       it "handles bad timestamp" $
         parseMessage "I xxxx mice in the air, I’m afraid, but you might catch a bat, and" `shouldBe` Unknown "I xxxx mice in the air, I’m afraid, but you might catch a bat, and"
-    describe "error" $
+    describe "error" $ do
       it "works" $
         parseMessage "E 2 562 help help" `shouldBe` LogMessage (Error 2) 562 "help help"
+      it "handles bad timestamp" $
+        parseMessage "E 404 xxxx mice in the air, I’m afraid, but you might catch a bat, and" `shouldBe` Unknown "E 404 xxxx mice in the air, I’m afraid, but you might catch a bat, and"
     describe "unknown" $
       it "passes string through" $
         parseMessage "Whatever" `shouldBe` Unknown "Whatever"
